156d28968344f1449adb07ab9696876705c3d00e
Ahyun Kim <zlzzlzz2l@naver.com>
2022-01-05 21:47:29 +0900

    Create: Springmvc Directory

---
+++ springmvc/src/test/java/com/lab/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
