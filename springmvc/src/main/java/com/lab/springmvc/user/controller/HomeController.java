f738df1a613c1d49b756a71fa7c17b91dae16169
Ahyun Kim <zlzzlzz2l@naver.com>
2022-01-09 20:10:51 +0900

    Create: 메인 컨트롤러 뷰 매핑 생성

---
+++ springmvc/src/main/java/com/lab/springmvc/user/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.lab.springmvc.user.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+    @GetMapping("/")
+    public String Home() {
+        return "index";
+    }
+}
