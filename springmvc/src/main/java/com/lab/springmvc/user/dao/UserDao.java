26e72f141a13fefec5a937dff7bc7498aa61ad08
Ahyun Kim <zlzzlzz2l@naver.com>
2022-01-10 23:48:49 +0900

    Create: 유저 Dao 로그인 추상메서드 추가

--- springmvc/src/main/java/com/lab/springmvc/user/dao/UserDao.java
+++ springmvc/src/main/java/com/lab/springmvc/user/dao/UserDao.java
@@ -2,6 +2,9 @@ package com.lab.springmvc.user.dao;
 
 import com.lab.springmvc.user.domain.User;
 
+import java.util.List;
+
 public interface UserDao {
     void insertMember(User user);
+    List<User> selectUserFromIdPw(String email, String pw);
 }
