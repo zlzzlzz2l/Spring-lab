7882ebf9e0cefd1b5503b87e2a25bcb46428ae53
Ahyun Kim <zlzzlzz2l@naver.com>
2022-01-11 23:35:16 +0900

    Refactor: 회원 엔티티 수정
    Auto Type에서 Identity로 수정했습니다.

--- springmvc/src/main/java/com/lab/springmvc/user/domain/User.java
+++ springmvc/src/main/java/com/lab/springmvc/user/domain/User.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Getter @Setter
 public class User {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
