937de29a0f6da8e2d3d1fda7ec490a1f7bd960ad
Ahyun Kim <zlzzlzz2l@naver.com>
2022-01-12 23:56:53 +0900

    Create: 게시판 Dao 구현클래스 생성

---
+++ springmvc/src/main/java/com/lab/springmvc/user/dao/BoardDaoImpl.java
@@ -0,0 +1,11 @@
+package com.lab.springmvc.user.dao;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class BoardDaoImpl {
+}
