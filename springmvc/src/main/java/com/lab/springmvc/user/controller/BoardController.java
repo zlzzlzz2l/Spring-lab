ad59ad23b8fa4f7655d84faecaf6d673a9f4e473
Ahyun Kim <zlzzlzz2l@naver.com>
2022-01-12 23:50:36 +0900

    Create: 게시판 컨트롤러 생성

---
+++ springmvc/src/main/java/com/lab/springmvc/user/controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.lab.springmvc.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+}
