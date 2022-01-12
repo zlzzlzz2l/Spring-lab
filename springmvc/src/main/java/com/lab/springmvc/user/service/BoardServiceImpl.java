05b5031b0a23ddf4dca4f246c58a6b36f9e46344
Ahyun Kim <zlzzlzz2l@naver.com>
2022-01-12 23:57:23 +0900

    Create: 게시판 서비스 구현클래스 생성

---
+++ springmvc/src/main/java/com/lab/springmvc/user/service/BoardServiceImpl.java
@@ -0,0 +1,11 @@
+package com.lab.springmvc.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BoardServiceImpl {
+}
